95e43e5eba4d43ceb3e399e68059dc38d29be715
Maxim Belyakov <58391051+maxim-belyakov@users.noreply.github.com>
2020-03-27 15:02:00 +0300

    cofiguring slider

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Link } from "react-router-dom";
-import { ButtonBack, ButtonFirst, ButtonLast, ButtonNext, CarouselProvider, Slide, Slider, } from 'pure-react-carousel';
+import { ButtonBack, ButtonNext, CarouselProvider, Slide, Slider, DotGroup } from 'pure-react-carousel';
 
 import projectsData from "../projectsData"
 
-import s from './../css/slider.css';
+import './../css/slider.scss';
+
+function eventLogger(ev) {
+  // eslint-disable-next-line no-console
+//   console.log(ev.type, ev.target);
+}
 
 export default function Portfolio() {
     const projects = projectsData.message
@@ -15,7 +20,7 @@ export default function Portfolio() {
           <Link to="/">Back</Link>
         </h2>
 
-        <div style={{display: "none"}}>
+        {/* <div style={{display: "none"}}>
             {projects.map((project, i) => (
                 <div key={i}>
                     <h3>{project.name}</h3>
@@ -28,43 +33,108 @@ export default function Portfolio() {
                     <a href={project.link}>{project.linkName}</a>
                 </div>
             ))}
-        </div>        
+        </div>*/}
 
         <div>
             <CarouselProvider
                 visibleSlides={1}
-                totalSlides={4}
+                totalSlides={7}
                 step={1}
-                naturalSlideWidth={400}
-                naturalSlideHeight={500}
+                // naturalSlideWidth={400}
+                // naturalSlideHeight={700}
                 isIntrinsicHeight
                 infinite={true}
             >
                 {/* <h2 className={s.headline}>With intrinsic axis dimension</h2> */}
 
-                <Slider className={s.slider}>
+                <Slider 
+                    className="slider" 
+                    trayProps={{
+                        // clipboard events? Sure why not.
+                        onCopy: eventLogger,
+                        onCut: eventLogger,
+                        onPaste: eventLogger,
+                
+                        // composition events
+                        onCompositionEnd: eventLogger,
+                        onCompositionStart: eventLogger,
+                        onCompositionUpdate: eventLogger,
+                
+                        // keyboard events
+                        onKeyDown: eventLogger,
+                        onKeyPress: eventLogger,
+                        onKeyUp: eventLogger,
+                
+                        // focus events,
+                        onFocus: eventLogger,
+                        onBlur: eventLogger,
+                
+                        // form events,
+                        onChange: eventLogger,
+                        onInput: eventLogger,
+                        onInvalid: eventLogger,
+                        onSubmit: eventLogger,
+                
+                        // mouse events
+                        onClick: eventLogger,
+                        onContextMenu: eventLogger,
+                        onDoubleClick: eventLogger,
+                        onDrag: eventLogger,
+                        onDragEnd: eventLogger,
+                        onDragEnter: eventLogger,
+                        onDragExit: eventLogger,
+                        onDragLeave: eventLogger,
+                        onDragOver: eventLogger,
+                        onDragStart: eventLogger,
+                        onDrop: eventLogger,
+                        onMouseDown: eventLogger,
+                        onMouseEnter: eventLogger,
+                        onMouseLeave: eventLogger,
+                        // onMouseMove: eventLogger,
+                        onMouseOut: eventLogger,
+                        onMouseOver: eventLogger,
+                        onMouseUp: eventLogger,
+                
+                        // touch events
+                        onTouchCancel: eventLogger,
+                        onTouchEnd: eventLogger,
+                        // onTouchMove: eventLogger,
+                        onTouchStart: eventLogger,
+                
+                        // pointer events
+                        onPointerDown: eventLogger,
+                        // onPointerMove: eventLogger,
+                        onPointerUp: eventLogger,
+                        onPointerCancel: eventLogger,
+                        onGotPointerCapture: eventLogger,
+                        onLostPointerCapture: eventLogger,
+                        onPointerEnter: eventLogger,
+                        onPointerLeave: eventLogger,
+                        onPointerOver: eventLogger,
+                        onPointerOut: eventLogger,
+                
+                        draggable: true,
+                      }}
+                >
 
-                {projects.map((project, i) => (                    
-                    <Slide index={i}>
-                        <div key={i}>
+                    {projects.map((project, i) => (                    
+                        <Slide index={i} key={i} className="slide">
                             <h3>{project.name}</h3>
                             <div className="content" style={{display: "block"}}>
                                 <p>{project.description}</p>
                                 {project.imgs.map((imgLink, i) => (
-                                    <img key={i} style={{width: "100%"}} alt={imgLink} src={'projects/' + imgLink}></img>
+                                    <img key={i} className="sliderImg" alt={imgLink} src={'projects/' + imgLink}></img>
                                 ))}                
                             </div>
                             <a href={project.link}>{project.linkName}</a>
-                            <img style={{width: "100%"}} src={'cats/img0'+ i + '.jpeg'}></img>
-                        </div>                        
-                    </Slide>
-                ))}
+                            <img  className="sliderImg" src={'cats/img0'+ i + '.jpeg'} alt={project.linkName}></img>                     
+                        </Slide>
+                    ))}
                 </Slider>
                 <div className="controllButtons">
-                    <ButtonFirst>First</ButtonFirst>
                     <ButtonBack>Back</ButtonBack>
                     <ButtonNext>Next</ButtonNext>
-                    <ButtonLast>Last</ButtonLast>
+                    <DotGroup dotNumbers />
                 </div>
             </CarouselProvider>
         
