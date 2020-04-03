4e940d3c838fd56cbe13946e2a7ea242f065afd8
Maxim Belyakov <58391051+maxim-belyakov@users.noreply.github.com>
2020-04-03 13:41:15 +0300

    changes in carousel; JSON and styles

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from "react-router-dom";
-import { ButtonBack, ButtonNext, CarouselProvider, Slide, Slider, DotGroup } from 'pure-react-carousel';
+import { ButtonBack, ButtonNext, CarouselProvider, Slide, Slider, DotGroup, Image } from 'pure-react-carousel';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons'
 
@@ -12,28 +12,40 @@ export default function Portfolio() {
     const projects = projectsData.message
 
     return(      
-        <div className="PortfolioContiner">
-            <main className="PortfolioContiner_Back">
+        <div className="Portfolio">
+            <main className="Portfolio_back">
                 <h2 className="check">
                     <Link to="/">Back</Link>
                 </h2>
             </main>
 
-            <div class="PortfolioContiner_Projects">
+            <div class="Portfolio_projects">
                 <CarouselProvider
+                    className="Portfolio_carousel"
                     visibleSlides={1}
                     totalSlides={7}
                     step={1}
-                    isIntrinsicHeight
-                    infinite={true}
                     currentSlide={1}
+                    // isIntrinsicHeight
+                    hasMasterSpinner={true}
+                    infinite={true}
+                    touchEnabled={true}
+                    dragEnabled={true}
                 >
-                    <Slider className="slider">
+                    <Slider 
+                        className="slider"
+                        classNameTray="carouselTray"
+                        moveThreshold={0.01}
+                        >
                         {projects.map((project, i) => (                    
-                            <Slide index={i} key={i} className="slide">
+                            <Slide 
+                                className="slide"
+                                index={i} 
+                                key={i} 
+                                >
                                 <div className="slideContent" className={"slideContent " + (i === 0 ? 'meText' : '')}>
                                     <div className="slideContent_picturesSlider">
-                                        <img className="slideContent_picture" alt={project.imgs[0]} src={'projects/' + project.imgs[0]}></img>
+                                        <Image className="slideContent_picture" alt={project.imgs[0]} src={'projects/' + project.imgs[0]} />
                                     </div>
                                     <div class="slideContent_text">
                                         <h3 className="slideContent_name">{project.name}</h3>
@@ -44,7 +56,7 @@ export default function Portfolio() {
 
                                         <div className="slideContent_tagContainer"> 
                                             {project.tags.map((tag, i) => (
-                                                <span className="slideContent_tag" style={{backgroundColor: tag.color}}>{tag.name}</span>
+                                                <span className="slideContent_tag ripple" style={{backgroundColor: tag.color}}>{tag.name}</span>
                                             ))}
                                         </div>
 
