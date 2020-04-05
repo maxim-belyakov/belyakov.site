6936ec6b91ec8c436fd16cbdec8b028dddbad644
Maxim Belyakov <58391051+maxim-belyakov@users.noreply.github.com>
2020-04-05 15:09:09 +0300

    fix About me, styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { BrowserRouter as Router, Route, Link, NavLink, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import { SocialIcon } from 'react-social-icons';
 import { CSSTransition } from "react-transition-group";
 
@@ -10,20 +10,10 @@ import './scss/app.scss';
 
 const routes = [
   { path: '/', name: 'Home', Component: Home },
-  { path: '/portfolio', name: 'Portfolio', Component: Portfolio }
+  { path: '/:portfolio/', name: 'Portfolio', Component: Portfolio }
 ]
 
 export default function App() {
-  let location = true;
-
-  // This piece of state is set when one of the
-  // gallery links is clicked. The `background` state
-  // is the location that we were at when one of
-  // the gallery links was clicked. If it's there,
-  // use it as the location for the <Switch> so
-  // we show the gallery in the background, behind
-  // the modal.
-  let background = location.state && location.state.background;
 
   return (
     <Router>
@@ -57,7 +47,7 @@ function Home() {
   return (
     <main>
       <h1>Maxim Belyakov</h1>
-      <p className="lead">My passion is creating, and I find happiness in being a better me today than the me yesterday.</p>
+      <p className="lead">Creating is my passion. Being better than yesterday really makes me happy.</p>
       <p className="quote">Life is 10% what happens to you and 90% how you react to it.</p>
       <p className="emoji"> 🚀 👽 👾 </p>
       <section>
