673cc691c52f097117bd1ccef72f8dc83723c268
maxim-belyakov <maxim.a.belyakov@gmail.com>
2021-07-20 17:25:03 +0300

    start of first page

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Link from "next/link";
+import styles from '../styles/Home.module.scss'
+
+import socialIcons from "../JSON/socialIcons"
 
 export default function Home() {
+  const socials = socialIcons.message
+  const iconSocialSize = 45
+
   return (
     <div className={styles.container}>
       <Head>
@@ -11,59 +17,26 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+      <main>
+        <h1>Maxim Belyakov</h1>
+        <p className={styles.lead}>Creating is my passion. Being better than yesterday really makes me happy.</p>
+        <p className={styles.quote}>Life is 10% what happens to you and 90% how you react to it.</p>
+        <p className={styles.emoji}>
+          <span role="img" aria-label="rocket">🚀</span>
+          <span role="img" aria-label="alien">👽</span>
+          <span role="img" aria-label="alien-monster">👾</span>
         </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <section>
+          <h2 className={styles.check}>
+            <Link href="/portfolio">Portfolio projects</Link>
+          </h2>
+        </section>
+        <section className={styles.socials}>
+          {/*{socials.map((social, i) => (*/}
+          {/*    <SocialIcon key={i} bgColor="#222222" target="_blank" style={{ height: iconSocialSize, width: iconSocialSize }} network={social.name} url={social.link} />*/}
+          {/*))}*/}
+        </section>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   )
 }
