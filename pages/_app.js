3a86198cd6f966af9e9df4af5b89245d223aab4f
maxim-belyakov <maxim.a.belyakov@gmail.com>
2021-08-20 14:13:12 +0300

    commented transition

--- pages/_app.js
+++ pages/_app.js
@@ -1,32 +1,10 @@
-import TransitionLayout from "../components/transition-layout/transition-layout";
+// import TransitionLayout from "../components/transition-layout/transition-layout";
 import '../styles/globals.css'
 
-import Router from "next/router";
-
 function MyApp({ Component, pageProps }) {
+
   return (
-    <TransitionLayout>
       <Component {...pageProps} />
-    </TransitionLayout>
   )
 }
-
-const routeChange = () => {
-  // Temporary fix to avoid flash of unstyled content
-  // during route transitions. Keep an eye on this
-  // issue and remove this code when resolved:
-  // https://github.com/vercel/next.js/issues/17464
-
-  const tempFix = () => {
-    const allStyleElems = document.querySelectorAll('style[media="x"]');
-    allStyleElems.forEach((elem) => {
-      elem.removeAttribute("media");
-    });
-  };
-  tempFix();
-};
-
-Router.events.on("routeChangeComplete", routeChange );
-Router.events.on("routeChangeStart", routeChange );
-
 export default MyApp
