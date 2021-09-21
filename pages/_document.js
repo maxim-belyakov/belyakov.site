4bc82e135287d86497fafa2f09ec1d295138c7fa
Maxim Belyakov <maxim.a.belyakov@gmail.com>
2021-09-21 14:54:18 +0300

    changed manifest

--- pages/_document.js
+++ pages/_document.js
@@ -12,12 +12,13 @@ export default class MyDocument extends Document {
           <Head>
             <meta name="Description"
                   content={'Maxim Belyakov - Hi, my name is Max 👋 I\'m a full time Senior Frontend Developer and part time entrepreneur.'}/>
-            <link rel="icon" href="/favicon.ico" />
-            <link rel="manifest" href="/manifest.json" />
             <link href='/favicon-16x16.png' rel='icon' type='image/png' sizes='16x16' />
             <link href='/favicon-32x32.png' rel='icon' type='image/png' sizes='32x32' />
+            <meta name="viewport" content="width=device-width, initial-scale=1"/>
             <link rel="apple-touch-icon" href="/apple-touch-icon.png"></link>
+            <link rel="manifest" href="/manifest.json" />
             <meta name="theme-color" content="#000000"/>
+            <link rel="icon" href="/favicon.ico" />
           </Head>
           <body>
             <Main />
